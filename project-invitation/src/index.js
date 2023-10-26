41b50fb456d7c0c60fe902d9f42f47df48f5b09a
= <=>
2023-10-26 15:29:01 +0700

    newupdate

--- project-invitation/src/index.js
+++ project-invitation/src/index.js
@@ -3,18 +3,27 @@ const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const app = express();
-const port = 3000;
+const port = 3200;
+var methodOverride = require('method-override');
+
 
 const route = require('./routes/indexRoute');
+const db = require('./config/db/indexDb');
+
+//connect to DB
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
     express.urlencoded({
         extended: true,
+        
     }),
 );
 app.use(express.json());
+
+app.use(methodOverride('_method'))
 
 // HTTP logger
 // app.use(morgan("combined"));
@@ -24,15 +33,18 @@ app.use(express.json());
     'hbs',
     engine({
         extname: '.hbs',
+        helpers:{
+            sum: (a, b) => a + b,
+        }
     }),
 );
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+app.set('views', path.join(__dirname, 'resources','views'));
 
 //Route init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
